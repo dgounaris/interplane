d4f41bd1ac0d398c55077a685dc28721e55b9f90
dgounaris <dimitris.gounaris@hotmail.com>
2018-06-01 13:22:06 +0200

    Fixed arithmetical errors

--- project.cpp
+++ project.cpp
@@ -83,17 +83,19 @@ int computeIntersections(set<EventInfo,leastEvent>& eventQ, SegmentInfo s1, Segm
     //if ( ((s2.c-s1.c) + (s1.b-s2.b)*(s1.b-s2.b)/(s1.a-s2.a)) / (s1.a-s2.a) < 0) {
     double intX1;
     double intX2;
+    int intC = 0;
     if (s1.a-s2.a == 0) {
         intX1 = (s2.c-s1.c)/(s1.b-s2.b);
         intX2 = intX1;
     }
-    double D = (s1.b-s2.b)*(s1.b-s2.b) - 4*(s1.a-s2.a)*(s1.c-s2.c);
-    if ( D < 0 ) {   
-        return 0;
+    else {
+        double D = (s1.b-s2.b)*(s1.b-s2.b) - 4*(s1.a-s2.a)*(s1.c-s2.c);
+        if ( D < 0 ) {   
+            return 0;
+        }
+        intX1 = (-(s1.b-s2.b) + sqrt(D))/(2*(s1.a-s2.a));
+        intX2 = (-(s1.b-s2.b) - sqrt(D))/(2*(s1.a-s2.a));
     }
-    int intC = 0;
-    intX1 = (-(s1.b-s2.b) + sqrt(D))/(2*(s1.a-s2.a));
-    intX2 = (-(s1.b-s2.b) - sqrt(D))/(2*(s1.a-s2.a));
     //double intX1 = sqrt( ((s2.c-s1.c) + (s1.b-s2.b)*(s1.b-s2.b)/(s1.a-s2.a)) / (s1.a-s2.a) ) - (s1.b-s2.b)/(2*(s1.a-s2.a));
     //double intX2 = -sqrt( ((s2.c-s1.c) + (s1.b-s2.b)*(s1.b-s2.b)/(s1.a-s2.a)) / (s1.a-s2.a) ) - (s1.b-s2.b)/(2*(s1.a-s2.a));
     //check if our intersection points are valid
